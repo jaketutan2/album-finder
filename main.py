d823a6d0669baa9e766b02d91541e1b55da3902c
jaketutan <jrt15971@comcast.net>
2022-08-02 17:11:16 -0600

    adding queue

--- main.py
+++ main.py
@@ -3,22 +3,28 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import os
+import random
+import sys
 
+def print_text():
+    iteration = 0;
 
-def print_hi(name):
     print('\n\n')
-    pause = ('spotify pause')
-    os.system(str(pause))
+    # pause = ('spotify pause')
+    # os.system(str(pause))
     # Use a breakpoint in the code line below to debug your script.
     print(f'\n\n\n-----------------------------------------------------------')
-    print(f'\tPress ENTER to Generate Random Album')
+    print(f'Press X to Exit')
+    print(f'-----------------------------------------------------------')
+    print(f'\n\tPress ENTER to Generate Random Album')
     print(f'\n\t\t\tOR ')
     print( f'\n\t\tFind Album by Decade')
     print(f'\n\t1 - 1950s | 2 - 1960s | 3 - 1970s | 4 - 1980s')
     print( f'\t5 - 1990s | 6 - 2000s | 7 - 2010s | 8 - 2020s')
     print(f'-----------------------------------------------------------\n\n\n')
 
-    keystroke = str(input())# Press Ctrl+F8 to toggle the breakpoint.
+def read_input():
+    keystroke = str(input())  # Press Ctrl+F8 to toggle the breakpoint.
     if keystroke == '1':
         x = '5'
     elif keystroke == '2':
@@ -28,49 +34,73 @@ def print_hi(name):
     elif keystroke == '4':
         x = '8'
     elif keystroke == '5':
-        x= '9'
+        x = '9'
     elif keystroke == '6':
         x = '0'
     elif keystroke == '7':
         x = '1'
     elif keystroke == '8':
         x = '2'
+    elif keystroke.lower() == 'x':
+        exit()
     else:
         x = 'all'
+    return x
+
+def get_data_frame():
     import pandas as pd
-    import random
 
-    data = pd.read_csv (r'C:\Users\jrt15\PycharmProjects\album-finder\AOTYdata.csv')
-    df = pd.DataFrame(data)
+    data = pd.read_csv(r'C:\Users\jrt15\PycharmProjects\album-finder\AOTYdata.csv')
+    return pd.DataFrame(data)
 
-    # Get Index
-    maxIndex = len(df.index)
-    index = random.randint(1, maxIndex)
+def restart():
+    os.execl(sys.executable, sys.executable, *sys.argv)
 
-    i = 0
+def find_album(data_frame, iteration):
+    max_index = len(data_frame.index)
+    index = random.randint(1, max_index);
+    album_iterator = 0
+    x = read_input()
     if x == 'all':
         print('\t')
-        print(*df.iloc[index, 0:4], sep='\n')
+        print(*data_frame.iloc[index, 0:4], sep='\n')
     else:
-        while i < maxIndex:
-            index = random.randint(1, maxIndex - 1)
-            year = str(df.iloc[index, 2])
+        while album_iterator < max_index:
+            index = random.randint(1, max_index - 1)
+            year = str(data_frame.iloc[index, 2])
             decade = year[2:3]
             if decade == x:
                 print('\t')
-                print(*df.iloc[index, 0:4], sep='\n')
+                print(*data_frame.iloc[index, 0:4], sep='\n')
                 break
-            i += 1
-    print('\n\n')
-    album = str(df.iloc[index, 1])
-    artist = str(df.iloc[index, 0])
-    command = ('spotify play --album ' + album + ' ' + artist)
-    os.system(str(command))
-
+            album_iterator += 1
     print(f'\n\n\n-------------------------------------------------------------')
+    play_spotify(index, iteration, data_frame)
+
+def play_spotify(index, iteration, data_frame):
+    print('\n\n')
+    album = str(data_frame.iloc[index, 1])
+    artist = str(data_frame.iloc[index, 0])
+    if iteration == 1:
+        print(f'\nENTER "y"')
+        os.system(str('spotify queue --album ' + album + ' ' + artist))
+        os.system(str('spotify next'))
+    else:
+        os.system(str('spotify queue --album ' + album + ' ' + artist))
+
+    print(f'\n\n\nITERATION ' + str(iteration))
+
+def generate_albums(iteration):
+    print_text()
+    df = get_data_frame()
+    find_album(df, iteration)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    i = 1
+    while i < 10:
+        generate_albums(i)
+        i = i + 1
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
